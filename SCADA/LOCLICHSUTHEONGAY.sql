e9641f7aa6c6c93e87091bbfe6ed45d3f6ddd1ab
Lê Hoàng <lehoang8900@gmail.com>
2025-06-16 18:39:45 +0700

    Add files via upload

---
+++ SCADA/LOCLICHSUTHEONGAY.sql
@@ -0,0 +1,15 @@
+﻿SELECT 
+    [id],  
+    [username],  
+    [Barcode], 
+    [Tên tài xế],
+    [Biển số xe], 
+    [Nhiên liệu],  
+    [Loại nhiên liệu], 
+    [Trạm cấp phát], 
+    [executed_at],   
+    [Ngày nhập nhiên liệu]
+FROM 
+    [lucdeptrai].[dbo].[query_history]
+WHERE 
+    executed_at >= '2025-06-13' AND executed_at < DATEADD(DAY, 1, '2025-06-13')
